7fd6c6c63c2c9904d2a1a720e9aac63eb1ce87a2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 10:53:37 -0700

    Update PublicHomepage.php

--- PublicHomepage.php
+++ PublicHomepage.php
@@ -1,15 +1,26 @@
 <?php
+
+function PublicHomepage(){
+	global $Title;
+	if(path(0)==false){
+		$Title = "Here's The Latest";
+	}else{
+		
+	}
+	TemplateBootstrap4($Title,'PublicHomepageBodyCallback();');
+}
+
 function PublicHomepageBodyCallback(){
-  ?>
-	
+	global $Title;
+?>
+
 <div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-      <h1>Welcome to the App</h1>
-      <p>This is the public homepage for users who are not logged in.</p>
-	  
-    </div>
-  </div>
+	<div class="row">
+		<div class="col-xs-12">
+			<h1><?php echo $Title; ?></h1>
+			
+		</div>
+	</div>
 </div>
 <?php
 }
