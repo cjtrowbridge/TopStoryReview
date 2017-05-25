26d1e42d8ee314fab812b66c2ad5dde1e0c34884
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 10:54:31 -0700

    Update PublicHomepage.php

--- PublicHomepage.php
+++ PublicHomepage.php
@@ -1,26 +1,27 @@
 <?php
 
 function PublicHomepage(){
-	global $Title;
-	if(path(0)==false){
-		$Title = "Here's The Latest";
-	}else{
-		
-	}
-	TemplateBootstrap4($Title,'PublicHomepageBodyCallback();');
+  global $Title;
+  if(path(0)==false){
+    $Title = "Here's The Latest";
+  }else{
+
+  }
+  TemplateBootstrap4($Title,'PublicHomepageBodyCallback();');
 }
 
 function PublicHomepageBodyCallback(){
-	global $Title;
-?>
+  global $Title;
+  ?>
 
-<div class="container">
-	<div class="row">
-		<div class="col-xs-12">
-			<h1><?php echo $Title; ?></h1>
-			
-		</div>
-	</div>
-</div>
-<?php
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <h1><?php echo $Title; ?></h1>
+        
+      </div>
+    </div>
+  </div>
+
+  <?php
 }
