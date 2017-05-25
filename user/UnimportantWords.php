1e7ac66619e9084f85455429422c5c67f8068c13
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-24 20:41:11 -0700

    Update UnimportantWords.php

--- user/UnimportantWords.php
+++ user/UnimportantWords.php
@@ -1,5 +1,14 @@
 <?php
 
 function UserPageUnimportantWords(){
-
+  $Words = Query("SELECT * FROM UnimportantWords");
+  foreach($Words as $Word){
+    ?>
+      
+      <div class="word">
+        <h3><a href="/word/<?php echo $Word['WordID']; ?>"><?php echo $Word['Word']; ?></a></h3>
+      </div>
+      
+    <?php
+  }
 }
