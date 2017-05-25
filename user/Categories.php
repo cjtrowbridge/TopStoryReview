e4bcde42601cc7d5f78355f50e285ca3d44f5e99
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-24 20:37:53 -0700

    Update Categories.php

--- user/Categories.php
+++ user/Categories.php
@@ -1,6 +1,17 @@
 <?php
 
 function UserPageCategories(){
-  $Category = Query("SELECT * FROM FeedCategory");
-  pd($Category);
+  $Categories = Query("SELECT * FROM FeedCategory");
+  foreach($Categories as $Category){
+    ?>
+    
+    <div class="category">
+      <h3><a href="/category/<?php echo $Category['FeedCategoryID']; ?>"><?php echo $Category['Name']; ?></a></h3>
+      <p><?php echo $Category['Description']; ?></p>
+      <p><?php echo $Category['Path']; ?></p>
+      <p><?php echo $Category['ParentID']; ?></p>
+    </div>
+    
+    <?php
+  }
 }
