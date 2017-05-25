c0fd1e45be91ba957800310ae76f7fc8d86f62d8
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-24 18:46:02 -0700

    Update UserHomepage.php

--- UserHomepage.php
+++ UserHomepage.php
@@ -1,14 +1,28 @@
 <?php
 
 function UserHomepageBodyCallback(){
+  include('user/UnimportantWords.php');
+  include('user/Sources.php');
+  include('user/Feeds.php');
+  include('user/Categories.php');
   ?>
 
 <div class="container">
   <div class="row no-gutters">
     <div class="col-md-12">
-      <h4>Welcome to the App</h4>
-      <p>This is the page a user sees when they log in.</p>
+      <h1>Control Center</h1>
       
+      <h2>Unimportant Words</h2>
+      <?php UserPageUnimportantWords(); ?>
+      
+      <h2>Sources</h2>
+      <?php UserPageSources(); ?>
+      
+      <h2>Feeds</h2>
+      <?php UserPageFeeds(); ?>
+      
+      <h2>Categories</h2>
+      <?php UserPageCategories(); ?>
       
     </div>
   </div>
