d15ab7bfe38bd0c7cd4cf474f9eec441d2b02400
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 11:03:35 -0700

    Update PublicHomepage.php

--- PublicHomepage.php
+++ PublicHomepage.php
@@ -1,24 +1,50 @@
 <?php
 
 function PublicHomepage(){
-  global $Title;
+  global $Title,$Category;
   if(path(0)==false){
-    $Title = "Here's The Latest";
+    $Title    = "Here's The Latest";
+    $Category = Query("SELECT * FROM FeedCategory WHERE Name LIKE 'Featured'");
   }else{
-
+  $Categories = Query('SELECT * FROM FeedCategory');
+    
+    $Category = null;
+    //$Path  = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],path(0));
+    foreach($Categories as $ThisCategory){
+      if(path(0)==$ThisCategory['Path']){
+        $Category = $ThisCategory;
+        break;
+      }
+    }
+    if($Category==null){
+      $Title = 'Not Found';
+    }else{
+      $Title = $Category['Name'];
+    }
   }
   TemplateBootstrap4($Title,'PublicHomepageBodyCallback();');
 }
 
 function PublicHomepageBodyCallback(){
-  global $Title;
+  global $Title,$Category;
   ?>
 
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
         <h1><?php echo $Title; ?></h1>
-        
+        <?php
+          if($Category==null){
+            //TODO log these
+            ?>
+            <p>Sorry about that.</p>
+            <?php
+          }else{
+            ?>
+              Here we go
+            <?php
+          }
+        ?>
       </div>
     </div>
   </div>
