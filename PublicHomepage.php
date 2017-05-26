b4fa6fa6358a4ac74fe088b605556964b3bc73f6
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 21:55:01 -0700

    Update PublicHomepage.php

--- PublicHomepage.php
+++ PublicHomepage.php
@@ -40,9 +40,9 @@ function PublicHomepageBodyCallback(){
             <?php
           }else{
             if($Category['Name']=='all'){
-              $Data = Query("SELECT Headline FROM Story WHERE PubDate > DATE_SUB(NOW(), INTERVAL 24 HOUR)"));
+              $Data = Query("SELECT Headline FROM Story WHERE PubDate > DATE_SUB(NOW(), INTERVAL 24 HOUR)");
             }else{
-              $Data = Query("SELECT Headline FROM Story WHERE FeedCategoryID = ".intval($Category['FeedCategoryID']." AND PubDate > DATE_SUB(NOW(), INTERVAL 24 HOUR)"));
+              $Data = Query("SELECT Headline FROM Story WHERE FeedCategoryID = ".intval($Category['FeedCategoryID'])." AND PubDate > DATE_SUB(NOW(), INTERVAL 24 HOUR)");
             }
             $Headlines = array();
             foreach($Data as $Headline){
