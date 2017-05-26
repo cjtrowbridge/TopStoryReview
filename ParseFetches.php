d1da4b96af0b4de595d50e45b3e64f4c59b7c199
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 18:34:46 -0700

    Update ParseFetches.php

--- ParseFetches.php
+++ ParseFetches.php
@@ -18,11 +18,20 @@ function SaveAllHeadlinePages(){
 
     $Headlines = PickBest($Headlines,5);
     $Content='';
+    $PReview='';
     foreach($Headlines as $Headline){
       $Content.="<p>".$Headline."</p>\n";
+      $Preview.=$Headline.' ';
     }
     $Permalink = '/'.$Category['Path'].'/'.date('Y-m-d-H');
-    Query("INSERT INTO HeadlineArchive (Permalink,Content,FeedCategoryID,DateTime)VALUES('".$Permalink."','".$Content."',".$Category['FeedCategoryID'].",NOW())");
+    
+    
+    $Scores = CondenseGetWordScores($Preview);
+    CondenseSortByScore($Scores, 'Score');
+    $Preview=$Scores[0]['Word'].', '.$Scores[1]['Word'].', '.$Scores[2]['Word'];
+    $Preview = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$Preview);
+      
+    Query("INSERT INTO HeadlineArchive (Permalink,Content,FeedCategoryID,DateTime)VALUES('".$Permalink."','".$Content."',".$Category['FeedCategoryID'].",NOW()),'".$Preview."'");
     
   }
 }
