fa3efc72aff03ca0a92c800a68c802829fb0db8f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 22:01:25 -0700

    Update PublicHomepage.php

--- PublicHomepage.php
+++ PublicHomepage.php
@@ -33,36 +33,7 @@ function PublicHomepageBodyCallback(){
       <div class="col-xs-12 col-md-9">
         <h1><?php echo $Title; ?></h1>
         <?php
-          if($Category==null){
-            //TODO log these
-            ?>
-            <p>Sorry about that.</p>
-            <?php
-          }else{
-            if($Category['Name']=='All'){
-              $Data = Query("SELECT Headline FROM Story WHERE PubDate > DATE_SUB(NOW(), INTERVAL 24 HOUR)");
-            }else{
-              $Data = Query("SELECT Headline FROM Story WHERE FeedCategoryID = ".intval($Category['FeedCategoryID'])." AND PubDate > DATE_SUB(NOW(), INTERVAL 24 HOUR)");
-            }
-            $Headlines = array();
-            foreach($Data as $Headline){
-              $Headlines[]=$Headline['Headline'];
-            }
-            
-            $NumberOfStories=5;
-            $Headlines = PickBest($Headlines,$NumberOfStories);
-            ?>
-            <p><i>Out of <?php echo count($Data); ?> headlines in category '<?php echo $Category['Name']; ?>,' I picked these <?php echo $NumberOfStories; ?> for you.</i></p>
-            <?php
-            foreach($Headlines as $Headline){
-              ?>
-        
-                <p><?php echo $Headline; ?></p>
-        
-              <?php
-            }
-            
-          }
+          echo PickTopStories($Category);
         ?>
       </div>
       <div class="col-xs-12 col-md-3">
