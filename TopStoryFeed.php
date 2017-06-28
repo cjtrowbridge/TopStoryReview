bdde21d6b56d790b331ef882bc6abd4c49025aab
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-06-28 10:46:21 -0700

    Update TopStoryFeed.php

--- TopStoryFeed.php
+++ TopStoryFeed.php
@@ -1,1 +1,17 @@
+<?php
 
+function TopStoryFeed($Category){
+  if($Category=='All'){
+    $Data = Query("SELECT Headline FROM Story WHERE PubDate > DATE_SUB(NOW(), INTERVAL 24 HOUR)");
+  }else{
+    $Data = Query("SELECT Headline FROM Story WHERE FeedCategoryID = ".intval($Category['FeedCategoryID'])." AND PubDate > DATE_SUB(NOW(), INTERVAL 24 HOUR)");
+  }
+  $Headlines = array();
+  foreach($Data as $Headline){
+    $Headlines[]=$Headline['Headline'];
+  }
+
+  $Headlines = PickBest2($Headlines,$NumberOfStories);
+
+  OutputJSON($Headlines);
+}
