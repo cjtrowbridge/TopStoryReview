c08d9fa522cbe8c7e8ecc5a96f975d9951199191
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-07-20 11:42:30 -0700

    Update Routing.php

--- Routing.php
+++ Routing.php
@@ -14,56 +14,8 @@ function TSRParser(){
   include('ParseFetches.php');
   TSRParseFetches();
   
-  /*
-  include('TopStoryFeed.php');
-  $Categories = GetTSRCategories();
-  foreach($Categories as $Category){
-    TopStoryFeed($Category['Path']);
-  }
-  */
   file_get_contents('https://topstoryreview.com/Build.php');
 }
-
-
-Hook('User Is Not Logged In - Before Presentation','PublicPageBefore();');
-function PublicPageBefore(){
- /*$Categories = Query("SELECT * FROM FeedCategory WHERE Name NOT LIKE 'Featured'");
-  foreach($Categories as $Category){
-    if($Category['ParentID']==''){
-      //Add Any Children
-      $Children = array();
-      foreach($Categories as $Child){
-        if($Child['ParentID']==$Category['FeedCategoryID']){
-          $Children[]=array(
-            'text' => $Child['Name'],
-            'link' => $Child['Path']
-          );
-        }
-      }
-      if(count($Children)>0){
-       array_unshift($Children, array(
-          'text' => $Category['Name'],
-          'link' => '/'.$Category['Path']
-        ), array(
-          'text' => 'divider',
-          'link' => ''
-        ));
-
-        $Type = 'dropdown';
-      }else{
-        $Type = 'link';
-      }
-      $Text = $Category['Name'];
-      $Path = '/'.$Category['Path'];
-      
-      Nav('main-not-logged-in',$Type,$Text,$Path,$Children);
-    }
-  }
-  */
-  //Nav('main-not-logged-in','link','Explore','/explore');
-  //Nav('main-not-logged-in','link','Login','/login');
-}
-
 
 Hook('User Is Not Logged In - Presentation','PublicPage();');
 function PublicPage(){
@@ -92,16 +44,7 @@ function PublicPage(){
   }
 }
 
-
-Hook('User Is Logged In - Before Presentation','UserPageBefore();');
-
-function UserPageBefore(){
-  //Nav('main-logged-in','link','Explore','/explore');
-}
-
-
 Hook('User Is Logged In - Presentation','UserPage();');
-
 function UserPage(){
   switch(path(0)){
     case 'word-scores':
