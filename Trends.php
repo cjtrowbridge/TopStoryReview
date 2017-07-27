3178e85f88e57d0e5cc791502c9a94f8ef978ad9
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-07-27 01:35:06 -0700

    Update Trends.php

--- Trends.php
+++ Trends.php
@@ -3,28 +3,47 @@
 function Trends($Period){
   switch($Period){
     case '24hr':
-      $Stories = Query('SELECT Headline FROM Story WHERE FetchDate > date_sub(now(),INTERVAL 24 HOUR)');
-      $Text = '';
-      foreach($Stories as $Story){
-        $Text.=$Story['Headline'].' ';
-      }
-      $Words = ScoreWords($Text);
-      $Words = array_slice($Words,0,10);
-      OutputJSON($Words);
-      exit;
+      ShowTrends('24 HOUR');
     case '1wk':
-      $Stories = Query('SELECT Headline FROM Story WHERE FetchDate > date_sub(now(),INTERVAL 1 WEEK)');
-      $Text = '';
-      foreach($Stories as $Story){
-        $Text.=$Story['Headline'].' ';
-      }
-      $Words = ScoreWords($Text);
-      $Words = array_slice($Words,0,10);
-      OutputJSON($Words);
-      exit;
+      ShowTrends('1 WEEK');
     default:
       
-      break;
+      die('add something about the options');
   }
+}
+
+ShowTrends($Period){
+  $Subpath = str_replace($Period,' ','');
+  
+  $Paths=array(
+    'archive/'.$Subpath,
+    'archive/'.$Subpath.'/'.date('Y'),
+    'archive/'.$Subpath.'/'.date('Y').'/'.date('m'),
+    'archive/'.$Subpath.'/'.date('Y').'/'.date('m').'/'.date('d')
+  );
+  foreach($Paths as $Path){
+    if(!(file_exists($Path))){
+      mkdir($Path);
+    }
+  }
+  
+  $ArchivePath = 'archive/trends/'.$Subpath.'/'.date('Y').'/'.date('m').'/'.date('d').'/'.date('H').':00:00.json';
+  
+  $Archive = ReadJSONArchive($ArchivePath);
+  if($Archive){
+    OutputJSON($Archive);
+    return;
+  }
+  
+  $Stories = Query('SELECT Headline FROM Story WHERE FetchDate > date_sub(now(),INTERVAL '.$Period.')');
+  $Text = '';
+  foreach($Stories as $Story){
+    $Text.=$Story['Headline'].' ';
+  }
+  $Words = ScoreWords($Text);
+  $Words = array_slice($Words,0,10);
+  OutputJSON($Words);
+  WriteJSONArchive($ArchivePath,$Words);
+  exit;
 }
 
